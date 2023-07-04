22ee6cd5feca0ab3de82cd550097e2cf84de04ab
AJGrey <greyajayi@yahoo.com>
2023-07-04 18:07:28 +0100

    car

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,13 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    const { _brand, _motor, _color } = this;
+    const Constructor = Object.getPrototypeOf(this).constructor;
+    return new Constructor(_brand, _motor, _color);
+  }
+}
